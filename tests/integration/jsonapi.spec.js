19add3faa9fb5334302c65379ea65c951e3175d8
Kirill Mokevnin <mokevnin@gmail.com>
2024-08-12 21:24:15 -0400

    update tests
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- tests/integration/jsonapi.spec.js
+++ tests/integration/jsonapi.spec.js
@@ -2,7 +2,7 @@
 
 import test, { expect } from "@playwright/test";
 
-test('should create a bug report', async ({ request }) => {
+test('posts:/id', async ({ request }) => {
   const response = await request.get('https://jsonplaceholder.typicode.com/posts/1')
   expect(response.ok()).toBeTruthy()
 
@@ -10,4 +10,20 @@ test('should create a bug report', async ({ request }) => {
   expect(data).toMatchObject(expect.objectContaining({
     "userId": 1
   }));
+});
+
+test('posts', async ({ request }) => {
+  const data = {
+    title: 'проекты хекслета',
+    body: 'рассказываем как с ними работать',
+    userId: 1,
+  }
+  const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
+    data,
+  })
+
+  expect(response.status()).toBe(201);
+
+  const responseData = await response.json();
+  expect(data).toMatchObject(expect.objectContaining(data));
 });
